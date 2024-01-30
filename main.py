a3f5aa5146410050b62630641894e71cc6986d09
TannedRose <smirnoffmir2010@gmail.com>
2024-01-30 17:16:11 +0300

    ,,,

--- main.py
+++ main.py
@@ -1,6 +1,120 @@
 # print(3, 4, 5, "heloo", sep="-")
 # print(3, 4, 5, "heloo")
 
-text = input("enter some text: ")
-print(text)
-a = None
+# text = input("enter some text: ")
+# print(text)
+# a = None
+# a = "-23_34"
+# b = int(a)
+#
+# a= "-34.561612"
+# b = float(a)
+# print(b)
+#
+#
+#
+# a = "23FFF"
+# print(int(a, 36))
+# a = 126
+# print(bin(a))
+#
+#
+# text = "Hello world"
+# a = len(text)
+# print(a)
+# text = "Python"
+# print(text[1:4])
+# print(str(True))
+# name = "Alex"
+# age = 34
+# data = {"first_name: name,"}
+# text = "Hello %(first_name)s %(user age)s" % (name, age)
+# print(text)
+# text = "Hello {}{}" format(name, age)
+# print(text)
+# text = f"Hello {name}{age + 2}"
+# print(text)
+
+# a = 42
+# b = str(a)
+# c = f"{a}"
+
+# text = "привет мир".encode()
+# text2 = "привет мир"
+# print(type(text))
+# print(text2)
+# text = u"hello"
+# print(text)
+
+# text = f"{3.587:.2f}"
+# print(text)
+
+# text = "hello "
+# print(text.capitalize())
+# print(text.lower())
+# print(text.swapcase())
+# print(text.casefold())
+# print(text.title())
+# print(text.upper())
+
+
+# text = "Hello world Python"
+# print(text."Hello World Python"int("llo"))
+# words = text.split(sep="---")
+# text2 = "-".join(words)
+# print(text2)
+# print(text.islower())
+# print(text.istitle())
+# print(text.isupper())
+
+# text = "Hello python python world"
+# print(text.partition("Python"))
+
+# text = "Hello python world"
+# print(text.replace())
+
+# text = "Hello world"
+# print(text.replace("", ""))
+
+# text = "Hello world"
+# print(text.removeprefix("Hell"))
+
+text = ("\t\tHello\t\tWorld\t\t\t")
+print(text.rstrip("\t\t"))
+
+# text = "Hello"
+# print(text.center(10,"-"))
+# print(text.ljust(10,"-"))
+# print(text.rjust(10,"-"))
+# print(text.zfill(10))
+
+# print("Hello".encode().decode())
+
+
+# print("Hello world".count("ell"))
+
+
+
+# print("Hello" in "Hello world")
+
+# a = 3600
+# b = 60 * 60
+# print(a is b)
+
+# print("H" < "h")
+
+# print(bin(15))
+# print(bin(14))
+# print(15 & 14)
+
+# print(15 ^ 14)
+
+# text = "Hello world"
+# print(text[2], text[~2])
+
+
+
+
+
+
+

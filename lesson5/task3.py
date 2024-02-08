c0b259b22f31a1c4a893f94c66cea6c58ba4e144
TannedRose <smirnoffmir2010@gmail.com>
2024-02-08 21:19:11 +0300

    ,,,

--- lesson5/task3.py
+++ lesson5/task3.py
@@ -1,11 +1,11 @@
 # Вывести четные числа от 2 до N по 5 в строку
 n = int(input("введите верхнюю границу "))
-for i in range(n):
-    if i % 2 == 0:
-        if i % 5 == 0:
-            print(i)
-        else:
-            print(i, end=",")
+for i in range(2, n, 2):
+    if i % 5 == 0:
+        print(i)
+    else:
+        print(i, end=",")
+
 
 
 

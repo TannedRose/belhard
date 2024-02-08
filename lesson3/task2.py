d5b8e73be564fff8291ae4f843f1ff362d64ca25
TannedRose <smirnoffmir2010@gmail.com>
2024-02-08 16:50:36 +0300

    ,,,

--- lesson3/task2.py
+++ lesson3/task2.py
@@ -1,9 +1,7 @@
 # Пользователь вводит 3 числа, найти среднее арифмитическое с точность 3
-number1 = input("введите первое число ")
-number2 = input("введите второе число ")
-number3 = input("введите третье число ")
-number1 = int(number1)
-number2 = int(number2)
-number3 = int(number3)
+number1 = int(input("введите первое число "))
+number2 = int(input("введите второе число "))
+number3 = int(input("введите третье число "))
+
 answer = (number1 + number2 + number3) / 3
 print(round(answer, 3))

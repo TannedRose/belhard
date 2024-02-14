17423565701ee23b305e76ace5ed06e10ff25dc3
TannedRose <smirnoffmir2010@gmail.com>
2024-02-14 19:13:15 +0300

    ,,,

--- lesson6/task9.py
+++ lesson6/task9.py
@@ -4,15 +4,19 @@
 # пустая строка)
 data = {
         1: {"name": "mike", "surname": 'kozlov', "fnumber": '+375291111111', "email": 'mikeK@gmail.com'},
-        2: {"name": "liz", "surname": 'ivanova', "fnumber": '+375441111111', "email": ' '},
+        2: {"name": "liz", "surname": 'ivanova', "fnumber": '+375441111111', "email": None},
         3: {"name": "ivan", "surname": 'melnikov', "fnumber": '+375292222222', "email": ""},
-        4: {"name": "ivan2", "surname": 'melnikov', "fnumber": '+375293333333'},
-        5: {"name": "ivan3", "surname": 'melnikov', "fnumber": '+375294444444', "email": 'ugouvu'}
+        4: {"name": "pavel", "surname": None, "fnumber": '+375293333333'},
+        # 5: {"name": "maxim", "surname": '', "fnumber": '+375294444444', "email": 'ugouvu'}
        }
 
-for user_id, info_user in data.items():
-    if 'email' not in info_user or ('@' and '.' not in info_user['email']):
-            print(info_user['name'])
+for i in data.values():
+    if not data.get("email"):
+        print(data.get("name"))
+
+# for user_id, info_user in data.items():
+#     if 'email' not in info_user or ('@' and '.' not in info_user['email']):
+#             print(info_user['name'])
 
 
 

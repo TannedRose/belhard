9f2a231c19a64c1bd592aa7c19e12d1ec260c912
TannedRose <smirnoffmir2010@gmail.com>
2024-03-06 19:31:49 +0300

    ...

--- lesson12.py
+++ lesson12.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, update, delete, insert, and_, all_, or_, any_, func, Table, alias
+from sqlalchemy import select, update, delete, insert, and_, all_, or_, any_, func, Table, alias, INT
 from psycopg2.extras import NamedTupleConnection, NamedTupleCursor
 from enum import IntEnum
 import datetime
@@ -8,38 +8,6 @@ class Status(IntEnum):
     IN_PROGRESS = 1
     DONE = 2
 
-
-# with NamedTupleConnection(dsn="postgres://admin:admin@217.76.60.77:6666/admin") as connection:
-#     with connection.cursor() as cursor:  # type: NamedTupleCursor
-#         cursor.execute("""
-#             CREATE TABLE IF NOT EXISTS projects (
-#                 id SERIAL PRIMARY KEY ,
-#                 name VARCHAR ( 32 ) NOT NULL UNIQUE
-#             );
-#         """)
-#         cursor.execute("""
-#             CREATE TABLE IF NOT EXISTS task (
-#                 id SERIAL PRIMARY KEY ,
-#                 FOREIGN KEY (project_id) REFERENCES project(id),
-#                 title VARCHAR(32) NOT NULL,
-#                 description TEXT ,
-#                 start_date TIMESTAMP NOT NULL,
-#                 end_date TIMESTAMP NOT NULL,
-#                 FOREIGN KEY (author_id) REFERENCES users(id),
-#                 FOREIGN KEY (executor_id) REFERENCES users(e_id),
-#                 status ENUM NOT NULL
-#             );
-#         """)
-#         cursor.execute("""
-#                     CREATE TABLE IF NOT EXISTS users (
-#                         id SERIAL PRIMARY KEY ,
-#                         e_id SERIAL PRIMARY KEY
-#                     );
-#                 """)
-#         connection.commit()
-#
-# q = select(users).join()
-# q = filter
 
 from sqlalchemy import (
     Column,
@@ -59,29 +27,31 @@ class Base(DeclarativeBase):
 
 class project(Base):
     __tablename__ = 'projects'
-    id = Column(int, primary_key=True)
+    id = Column(INT, primary_key=False)
     name = Column(VARCHAR(32), nullable=False)
 
 class tasks(Base):
     __tablename__ = 'tasks'
-    id = Column(int, primary_key=True)
-    project_id = Column(int, ForeignKey(column="project.id"), nullable=True)
+    id = Column(INT, primary_key=True)
+    project_id = Column(INT, ForeignKey(column="project.id"), nullable=False)
     title = Column(TEXT)
-    start_date = Column(TIMESTAMP, nullable=True)
-    author_id = Column(int, ForeignKey(column="users.id"), nullable=True)
-    executor_id = Column(int, ForeignKey(column="users.e_id"), nullable=True)
-    status = Column(Status, nullable=True)
+    start_date = Column(TIMESTAMP, nullable=False)
+    author_id = Column(INT, ForeignKey(column="users.id"), nullable=False)
+    executor_id = Column(INT, ForeignKey(column="users.e_id"), nullable=False)
+    status = Column(Status, nullable=False)
 
 class users(Base):
-    id = Column(int, primary_key=True)
-    e_id = Column(int, primary_key=True)
+    id = Column(INT, primary_key=False)
+    e_id = Column(INT, primary_key=False)
 
 q = select(users).join(tasks)
 q = q.filter(
     or_(
-        status != 2
+        tasks.status != 2
     )
     (
-        datetime.date < end_date
+        datetime.date < tasks.end_date
     )
 )
+
+
